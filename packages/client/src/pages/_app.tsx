36114caa1648168696869a8acd5e685db8f4eee3
kang-jin <dmssk13@gmail.com>
2023-03-05 23:55:50 +0900

    모노레포 구조 변경

--- packages/client/src/pages/_app.tsx
+++ packages/client/src/pages/_app.tsx
@@ -1,6 +1,8 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import common from "common";
 
 export default function App({ Component, pageProps }: AppProps) {
+  common.test();
   return <Component {...pageProps} />
 }
